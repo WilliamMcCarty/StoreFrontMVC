90b2d20199dcaf8de905a95efd21dafbc505ce0c
William McCarty <kcchief@yahoo.com>
2021-08-31 09:32:20 -0500

    Added gitignore file and added configs folder

--- StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+using StoreFront.UI.MVC;
 
-namespace IdentitySample.Controllers
+namespace IdentitySample.Controllers    
 {
     public class HomeController : Controller
     {
@@ -23,6 +27,39 @@ namespace IdentitySample.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string myMessage = $"Message received from Floral Gifts & Collectibles.  From {cvm.Name} - Phone: {cvm.Phone} - Email: {cvm.Email} <br/>Message: {cvm.Message}";
+            MailMessage mailMessage = new MailMessage(
+            ConfigurationManager.AppSettings["EmailUser"].ToString(),
+            ConfigurationManager.AppSettings["EmailTo"].ToString(),
+            "Message From Floral Gifts & Collectibles Contact Form",
+            myMessage);
+            mailMessage.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+            client.Port = 8889;
+            try
+            {
+                client.Send(mailMessage);
+                ViewBag.SuccessMessage = " - Message Sent.";
+            }
+            catch (System.Exception)
+            {
+                ViewBag.SuccessMessage = "Error: Please try again later.";
+                return View(cvm);
+            }
 
             return View();
         }
